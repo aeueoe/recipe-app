b99a3f763431b72881f5b9cdb494f7cb13f9c8f6
aeueoe <drom.jul@gmail.com>
2024-10-14 13:17:54 +0200

    task 2.5

--- recipes/models.py
+++ recipes/models.py
@@ -1,11 +1,28 @@
 from django.db import models
+from django.shortcuts import reverse
 
 class Recipe(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    cooking_time = models.CharField(max_length=50)
-    ingredients = models.TextField()
+    name = models.CharField(max_length=120)
+    cooking_time = models.FloatField(help_text='In minutes')
+    ingredients = models.CharField(max_length=350)
     description = models.TextField()
+    pic = models.ImageField(upload_to='recipes', default='no_image.jpg')
+
+    @property
+    def difficulty(self):
+        ingredients = self.ingredients.split(', ')
+        cooking_time_minutes = float(self.cooking_time)
+        if cooking_time_minutes < 30 and len(ingredients) < 7:
+            return 'Easy'
+        elif cooking_time_minutes < 30 and len(ingredients) >= 7:
+            return 'Medium'
+        elif cooking_time_minutes >= 30 and len(ingredients) < 7:
+            return 'Intermediate'
+        elif cooking_time_minutes >= 30 and len(ingredients) >= 7:
+            return 'Hard'
+
+    def get_absolute_url(self):
+        return reverse('recipes:details', kwargs={'pk': self.pk})
 
     def __str__(self):
-        return self.name
+        return str(self.name)
--- recipes/tests.py
+++ recipes/tests.py
@@ -1,19 +1,32 @@
 from django.test import TestCase
 from .models import Recipe
 
-class RecipeModelTestCase(TestCase):
-    def test_recipe_name_max_length(self):
-        recipe = Recipe(name='A' * 255, cooking_time='30 minutes', ingredients='Test ingredients', description='Test description')
-        recipe.full_clean()
+class RecipeModelTest(TestCase):
 
-    def test_recipe_cooking_time_max_length(self):
-        recipe = Recipe(name='Test recipe', cooking_time='A' * 50, ingredients='Test ingredients', description='Test description')
-        recipe.full_clean()
+    def setUpTestData():
+        # Set up non-modified objects used by all test methods
+        Recipe.objects.create(name='Tea', cooking_time=5, ingredients='tea-leaves, water, sugar',
+        description='Add tea leaves to boiling water, then add sugar')
 
-    def test_recipe_ingredients_text_field(self):
-        recipe = Recipe(name='Test recipe', cooking_time='30 minutes', ingredients='This is a test recipe', description='Test description')
-        self.assertEqual(recipe.ingredients, 'This is a test recipe')
+    def test_description(self):
+        recipe = Recipe.objects.get(id=1)
+        name_max_length = recipe._meta.get_field('name').max_length
+        self.assertEqual(name_max_length, 120)
 
-    def test_recipe_description_text_field(self):
-        recipe = Recipe(name='Test recipe', cooking_time='30 minutes', ingredients='Test ingredients', description='This is a test recipe description')
-        self.assertEqual(recipe.description, 'This is a test recipe description')
+    def test_recipe_name(self):
+        recipe = Recipe.objects.get(id=1)
+        recipe_name_label = recipe._meta.get_field('name').verbose_name
+        self.assertEqual(recipe_name_label, 'name')
+
+    def test_cookingtime_helptext(self):
+        recipe = Recipe.objects.get(id=1)
+        recipe_cookingtime = recipe._meta.get_field('cooking_time').help_text
+        self.assertEqual(recipe_cookingtime, 'In minutes')
+
+    def test_get_absolute_url(self):
+        recipe = Recipe.objects.get(id=1)
+        self.assertEqual(recipe.get_absolute_url(), '/list/1/')
+
+    def test_difficulty(self):
+        recipe = Recipe.objects.get(id=1)
+        self.assertEqual(recipe.difficulty, 'Easy')
